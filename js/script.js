72e5a5a5724e64a51cd6e46e65bc59df0b590db9
AlbertoSardo <sardoalbi@gmail.com>
2025-02-25 23:10:58 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
     const alertDiv = document.getElementById("entrybutton");
     const textInput = document.getElementById("entryinput");
     const resultHeading = document.getElementById("textoutput");
 
-    const userName = "Alberto Sardo"; 
-
+    const userName = "Alberto"; 
+    
     alertDiv.addEventListener("click", function () {
-
-        const inputValue = textInput.value;
-
-        alert(`${userName}: ${inputValue}`);
         
-        resultHeading.textContent = inputValue;
+        const inputValue = textInput.value.trim();
+
+        if (inputValue) {
+            
+            alert(`${userName}: ${inputValue}`);
+
+            resultHeading.textContent = inputValue;
+        } else {
+            alert("Please enter a message before clicking 'Alert Me'.");
+        }
     });
 });
 
